9dbc7f870c1a54e1cbd5daede8fa38aac5fbe415
Lailz <lailaabdulraheem@gmail.com>
2020-12-10 16:26:55 +0300

    remove body-parser

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const db = require("./db/models");
 const cors = require("cors");
-const bodyParser = require("body-parser");
 const thingRoutes = require("./api/things/routes");
 
 const app = express();
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
 app.use("/things", thingRoutes);
