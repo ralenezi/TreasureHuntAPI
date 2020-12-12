6556d91e8681a3c2bd530a3891ec64903aec6bc3
Rahaf Alenezi <irahaf@live.it>
2020-12-12 18:35:31 +0300

    first commit

---
+++ db/models/User.js
@@ -0,0 +1,13 @@
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  })
+  return User
+}
