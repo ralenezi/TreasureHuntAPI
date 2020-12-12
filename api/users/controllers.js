190a473c8511c75041156e2008eafe76a5a5327d
Rahaf Alenezi <irahaf@live.it>
2020-12-12 19:47:19 +0300

    sign up updated

--- api/users/controllers.js
+++ api/users/controllers.js
@@ -1,10 +1,15 @@
 const { User } = require('../../db/models')
+const bcrypt = require('bcrypt')
 
 exports.signup = async (req, res, next) => {
+  const { password } = req.body
+  const saltRounds = 10
   try {
+    const hashedPassword = await bcrypt.hash(password, saltRounds)
+    req.body.password = hashedPassword
     const newUser = await User.create(req.body)
-    res.json(newUser)
+    res.status(201).json({ message: 'User created successfully', newUser })
   } catch (error) {
-    console.log('here is an error at controller.js', error)
+    res.json({ message: 'here is an error at controller.js', error })
   }
 }
